d6f301eaaa7e56446dd6a90c17ee03f0d9e06aa3
SaadYousaf <saadyousaf@A006-00314.local>
2021-02-11 16:29:48 +0500

    [TNL-7729] - Add check to discussion rest API to prevent users in blackout period.

--- lms/djangoapps/discussion/rest_api/exceptions.py
+++ lms/djangoapps/discussion/rest_api/exceptions.py
@@ -2,6 +2,7 @@
 
 
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.exceptions import APIException
 
 
 class DiscussionDisabledError(ObjectDoesNotExist):
@@ -17,3 +18,8 @@ class ThreadNotFoundError(ObjectDoesNotExist):
 class CommentNotFoundError(ObjectDoesNotExist):
     """ Comment was not found. """
     pass  # lint-amnesty, pylint: disable=unnecessary-pass
+
+class DiscussionBlackedOutException(APIException):
+    """ Discussions are in blackout period. """
+    status_code = 403
+    default_detail = 'Discussions are in black out period.'
---
+++ lms/djangoapps/discussion/rest_api/utils.py
@@ -0,0 +1,17 @@
+"""
+Utils for discussion API.
+"""
+
+from lms.djangoapps.discussion.django_comment_client.utils import has_discussion_privileges
+
+
+def discussion_closed_for_user(course, user):
+    """
+    Check if course discussion are closed or not for user.
+    :param course:
+    :param user:
+    :return:
+    """
+    if not course.forum_posts_allowed and not has_discussion_privileges(user, course.id):
+        return True
+    return False
